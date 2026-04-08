978dea2d2adb43d75dd778a8c00a1c405ef1ddca
Angela Venable <avenable11@ivytech.edu>
2026-04-07 20:17:22 -0400

    with m04-a lab

--- 04_07_26/main.cpp
+++ 04_07_26/main.cpp
@@ -2,17 +2,49 @@
 #include <string>
 #include <limits>
 
+const int DEFAULT = 0;
 void resetStream();
 int factorial(int n);
 
 int inputInt(std::string prompt, std::string error, bool (*valid)(int, int, int), int lower = 0, int upper = 0);
 bool intInRange(int n, int lower, int upper);
-bool intGTX(int n, int x, int = 0);
+bool intGTX(int n, int x, int = DEFAULT);
+bool intLTX(int n, int x, int = DEFAULT);
+bool intGTEQ(int n, int x, int = DEFAULT);
+bool intLTEQ(int n, int x, int = DEFAULT);
+long fibNum(long f[], long n);
+
+// M04 part A lab
+// use the fibonacci number code from page 296 of the textbook
+// implement as a function
+// compare with recursive version
+// which do you prefer and why?
+// submit your preference and the function
 
 int main()
 {
-    int n = inputInt("Enter a positive number: ", "That is not a positive number!", intGTX, -1);
+    std::string prompt = "Enter a number greater than 0: ";
+    std::string err = "Please enter a number greater than 0.";
+    int n = inputInt(prompt, err, intGTX, -1);
     std::cout << factorial(n) << std::endl;
+    prompt = "Enter the first fibonacci number: ";
+    err = "The number entered must be greater than 0.";
+    long fibNum1 = inputInt(prompt, err, intGTX);
+    prompt = "Enter the second fibonacci number: ";
+    err = "The number entered must be greater than or equal to " + std::to_string(fibNum1);
+    long fibNum2 = inputInt(prompt, err, intGTEQ, fibNum1);
+    prompt = "Enter the position of the desired fibonacci number: ";
+    err = "The number entered must be greater than 0.";
+    long nthFibonacci = inputInt(prompt, err, intGTX);
+    long *fibSeq = new long[nthFibonacci];
+    for (int i = 0; i < nthFibonacci; i++)
+    {
+        fibSeq[i] = -1;
+    }
+    fibSeq[0] = fibNum1;
+    fibSeq[1] = fibNum2;
+
+    std::cout << "The " << nthFibonacci << "th Fibonacci number is " << fibNum(fibSeq, nthFibonacci) << std::endl;
 
     return 0;
 }
@@ -95,3 +127,32 @@ bool intGTX(int n, int x, int)
     }
     return f;
 } */
+
+bool intLTX(int n, int x, int)
+{
+    return n < x;
+}
+
+bool intGTEQ(int n, int x, int)
+{
+    return n >= x;
+}
+
+bool intLTEQ(int n, int x, int)
+{
+    return n <= x;
+}
+
+long fibNum(long f[], long n)
+{
+    if (n <= 0)
+    {
+        return 0;
+    }
+    if (f[n - 1] != -1)
+    {
+        return f[n - 1];
+    }
+    f[n - 1] = fibNum(f, n - 1) + fibNum(f, n - 2);
+    return f[n - 1];
+}
